685e2acd38b113556a903ee3e941338c59f72359
Tia <munenetiara@gmail.com>
2025-06-19 15:51:51 +0300

    Update fare.js

--- bodaFareCalculator/fare.js
+++ bodaFareCalculator/fare.js
@@ -1,16 +1,23 @@
 console.log("Challenge 2: Boda Boda Ride Fare Estimator")
 
+// Prompt user for distance in kilometers
 const fareInput = prompt("Unafika wapi Mkubwa? Kilometer ngapi?:");
 const distance= Number (fareInput);
 
+// Makes Input to be valid
 if (isNaN(distance)|| distance <= 0){
     console.log("Not an appropriate Distance")
+    
 } else{
+    // Fare calculation constants
     const baseFare= 50;
     const perKm= 15;
+
+    // Calculate fare components
     let distancePerkm = distance*perKm;
-    let total = baseFare + distance * perKm;
+    let total = baseFare + (distance * perKm);
     
+    // Outputs of fare 
     console.log(`Uko kwote? Io ni ${distance} km:`); 
     console.log(`Ukikalia Pikipiki: KES ${baseFare}`); 
     console.log(`Mpaka Uko: KES ${distancePerkm}`);
@@ -19,10 +26,12 @@ if (isNaN(distance)|| distance <= 0){
 }
 
 
-
+// Function to calculate total boda fare 
 function calculateBodaFare(distance){
     return  baseFare + (distance * perKm);
     
 }
+
+
 
 
